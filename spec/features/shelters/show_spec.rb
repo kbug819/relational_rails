7e9c032aaf0fc4095496951383f8bed24d789552
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-24 14:29:45 -0500

    feat: user story 10 feat and testing

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -48,4 +48,19 @@ RSpec.describe "Disaster Shelter Show", type: :feature do
       end
     end
   end
+
+  describe "10. As a visitor" do
+    describe "when I visi a shelter show page" do
+      it "I see a link to take me to the resident of that shelter's page" do
+        arlington_life = Shelter.create!(shelter_name: "Arlington Life Shelter", address: "lkahlsdkfh", case_management_available: true, capasity: 50)
+        austin_street = Shelter.create!(shelter_name: "Austin Street Shelter", address: "lkahlsdkfh", case_management_available: false, capasity: 25)
+        jones = arlington_life.residents.create!(family_name: "Jones", long_term_housing_need: true, family_size: 4)
+        bradley = austin_street.residents.create!(family_name: "Bradley", long_term_housing_need: false, family_size: 2) 
+
+        visit "/shelters/#{arlington_life.id}"
+
+        expect(page).to have_content("Current Residents At This Shelter")
+      end
+    end
+  end
 end
