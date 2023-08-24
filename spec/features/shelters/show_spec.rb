40aa205e0d40f887730a06b647672cf7168ab1c1
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-24 12:37:17 -0500

    user story 7 test setup

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -33,4 +33,19 @@ RSpec.describe "Disaster Shelter Show", type: :feature do
       end
     end
   end
+
+  describe "7. As a visitor" do
+    describe "when I visit a shelter's show page" do
+      it "I see a countt of the number of residents associated with this parent" do
+        arlington_life = Shelter.create!(shelter_name: "Arlington Life Shelter", address: "lkahlsdkfh", case_management_available: true, capasity: 50)
+        austin_street = Shelter.create!(shelter_name: "Austin Street Shelter", address: "lkahlsdkfh", case_management_available: false, capasity: 25)
+        jones = arlington_life.residents.create!(family_name: "Jones", long_term_housing_need: true, family_size: 4)
+        bradley = austin_street.residents.create!(family_name: "Bradley", long_term_housing_need: false, family_size: 2) 
+
+        visit "/shelters/#{arlington_life.id}"
+
+        expect(page).to have_content("Residents = 1")
+      end
+    end
+  end
 end
