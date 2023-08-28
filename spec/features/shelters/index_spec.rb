5e962dc833c2957d82fe8c5f06667d25f12ddc0c
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-27 22:01:03 -0500

    testing update for user testing 17

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -75,9 +75,22 @@ RSpec.describe "Disaster Shelter Index", type: :feature do
     describe "I can visit the index page and see an edit button next to each shelter" do
       it "will take the user to the edit page for that shelter" do
         arlington_life = Shelter.create!(shelter_name: "Arlington Life Shelter", address: "lkahlsdkfh", case_management_available: true, capasity: 50)
-        austin_street = Shelter.create!(shelter_name: "Austin Street Shelter", address: "lkahlsdkfh", case_management_available: false, capasity: 25)
         visit "/shelters"
         expect(page).to have_content("Edit Shelter Record")
+        click_link("Edit Shelter Record")
+        expect(page).to have_content("Shelter Name")
+        expect(page).to have_content("Address")
+        expect(page).to have_content("Is Case Management Available")
+        expect(page).to have_content("Capasity")
+        fill_in('shelter_name', with: "Test1")
+        fill_in('address', with: "addresstest")
+        fill_in('case_management_available', with: "true")
+        fill_in('capasity', with: "30")
+        click_button('Update Shelter Record')
+        expect(page).to have_content("Test1")
+        expect(page).to have_content("Shelter Address: addresstest")
+        expect(page).to have_content("Case Management Available: true")
+        expect(page).to have_content("Capasity: 30")
       end
     end
   end
