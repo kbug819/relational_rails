e92cb189e3bdefa0c04ddaed5ed5d93a156b7cd3
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-27 19:56:44 -0500

    user story 5 test update and review

--- spec/features/residents/show_spec.rb
+++ spec/features/residents/show_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Resident Show", type: :feature do
-  describe "4. As a visitor" do
-    describe "when I visit '/residents/:id" do
-      it "then I see the resident with that id including the residen's attributes" do
+  describe "4. As a visitor when I visit '/residents/:id" do
+    it "then I see the resident with that id including the residen's attributes" do
 
-        arlington_life = Shelter.create!(shelter_name: "Arlington Life Shelter", address: "lkahlsdkfh", case_management_available: true, capasity: 50)
-        austin_street = Shelter.create!(shelter_name: "Austin Street Shelter", address: "lkahlsdkfh", case_management_available: false, capasity: 25) 
-        jones = Resident.create!(family_name: "Jones", long_term_housing_need: true, family_size: 4, shelter_id: "#{arlington_life.id}")
-        bradley = Resident.create!(family_name: "Bradley", long_term_housing_need: false, family_size: 2, shelter_id: "#{austin_street.id}")
-        visit "/residents/#{jones.id}" 
+      arlington_life = Shelter.create!(shelter_name: "Arlington Life Shelter", address: "lkahlsdkfh", case_management_available: true, capasity: 50)
+      austin_street = Shelter.create!(shelter_name: "Austin Street Shelter", address: "lkahlsdkfh", case_management_available: false, capasity: 25) 
+      jones = Resident.create!(family_name: "Jones", long_term_housing_need: true, family_size: 4, shelter_id: "#{arlington_life.id}")
+      bradley = Resident.create!(family_name: "Bradley", long_term_housing_need: false, family_size: 2, shelter_id: "#{austin_street.id}")
+      visit "/residents/#{jones.id}" 
 
-        expect(page).to have_content(jones.family_name)
-        expect(page).to have_content("Long Term Housing Need: #{jones.long_term_housing_need}")
-        expect(page).to have_content("Family Size: #{jones.family_size}")
-        expect(page).to have_content("Shelter ID: #{jones.shelter_id}")
-      end
+      expect(page).to have_content(jones.family_name)
+      expect(page).to have_content("Long Term Housing Need: #{jones.long_term_housing_need}")
+      expect(page).to have_content("Family Size: #{jones.family_size}")
+      expect(page).to have_content("Shelter ID: #{jones.shelter_id}")
       
+      visit "/residents/#{bradley.id}" 
+      expect(page).to have_content(bradley.family_name)
+      expect(page).to have_content("Long Term Housing Need: #{bradley.long_term_housing_need}")
+      expect(page).to have_content("Family Size: #{bradley.family_size}")
+      expect(page).to have_content("Shelter ID: #{bradley.shelter_id}")
     end
   end
 
