d56b20f82e9885ddf2e9d4f3964ba03b1ff27421
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-27 22:19:58 -0500

    update testing for user story 19

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -97,11 +97,13 @@ RSpec.describe "Disaster Shelter Show", type: :feature do
     it "enables the user to permanently delete a record" do
         arlington_life = Shelter.create!(shelter_name: "Arlington Life Shelter", address: "lkahlsdkfh", case_management_available: true, capasity: 50)
         austin_street = Shelter.create!(shelter_name: "Austin Street Shelter", address: "lkahlsdkfh", case_management_available: false, capasity: 25)
-        jones = arlington_life.residents.create!(family_name: "Jones", long_term_housing_need: true, family_size: 4)
-        bradley = austin_street.residents.create!(family_name: "Bradley", long_term_housing_need: false, family_size: 2) 
-
+        
+        visit "/shelters"
+        expect(page).to have_content(arlington_life.shelter_name)
         visit "/shelters/#{arlington_life.id}"
         expect(page).to have_button("delete")
+        click_button('delete')
+        expect(page).not_to have_content(arlington_life.shelter_name)
     end
   end
 end
