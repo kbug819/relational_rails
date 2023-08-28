48fb3cc25257a0c339e382434b518c50fe2134c1
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-27 21:47:18 -0500

    update user testing for story 16

--- spec/features/residents/index_spec.rb
+++ spec/features/residents/index_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Resident Index", type: :feature do
   describe "3. As a Visitor when I visit /residents" do
-    xit "I see the name of each resident record in the system" do
+    it "I see the name of each resident record in the system" do
 
       arlington_life = Shelter.create!(shelter_name: "Arlington Life Shelter", address: "lkahlsdkfh", case_management_available: true, capasity: 50)
       austin_street = Shelter.create!(shelter_name: "Austin Street Shelter", address: "lkahlsdkfh", case_management_available: false, capasity: 25) 
       jones = Resident.create!(family_name: "Jones", long_term_housing_need: true, family_size: 4, shelter_id: "#{arlington_life.id}")
-      bradley = Resident.create!(family_name: "Bradley", long_term_housing_need: false, family_size: 2, shelter_id: "#{austin_street.id}")
+      bradley = Resident.create!(family_name: "Bradley", long_term_housing_need: true, family_size: 2, shelter_id: "#{austin_street.id}")
       
       visit "/residents"
 
