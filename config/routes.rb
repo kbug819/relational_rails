7377b1a0ba04f6121509ec8becfe816ee9b63fbe
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-28 17:20:38 -0500

    routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -7,24 +7,19 @@ Rails.application.routes.draw do
   get "/", to: "welcome#index"
   get "/shelters", to: "shelters#index"
   get "/shelters/new", to: "shelters#new"
-  get "/shelters/:id", to: "shelters#show"
-  get "/residents", to: "residents#index"
-  get "/residents/:id", to: "residents#show"
-  get "/shelters/:id/residents", to: "shelter_residents#index"
-  # get "/shelters/:id/residents", to: "shelters#resident_view"
   post "/shelters", to: "shelters#create"
+  get "/shelters/:id", to: "shelters#show"
   get "/shelters/:id/edit", to: "shelters#edit"
   patch "/shelters/:id", to: 'shelters#update'
+  delete "/shelters/:id", to: 'shelters#destroy'
+  
+  get "/shelters/:id/residents", to: "shelter_residents#index"
   get "/shelters/:id/residents/new", to: "shelter_residents#new"
   post "/shelters/:id/new", to: "shelter_residents#create"
+
+  get "/residents", to: "residents#index"
+  get "/residents/:id", to: "residents#show"
   get "/residents/:id/edit", to: "residents#edit"
   patch "residents/:id/", to: 'residents#update'
-  # get "/shelters/:id/residents/sorted_a", to: "shelter_residents#sorted_a"
-  delete "/shelters/:id", to: 'shelters#destroy'
   delete "/residents/:id", to: 'residents#destroy'
-  post "/shelters/:id/residents", to: 'shelter_residents#index'
-  get "/shelters/:id/residents", to: "shelter_residents#index"
-
-
-  # get "/shelters/:id/residents/by_family_view", to: 'shelter_residents#by_family_view'
 end
