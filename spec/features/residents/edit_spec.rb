b57551018f66d4ea9a7881ae966c2391809de299
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-27 22:09:09 -0500

    updated user testing for story 18

--- spec/features/residents/edit_spec.rb
+++ spec/features/residents/edit_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Resident Show", type: :feature do
-
+  describe "18 As a visitor" do
+    describe "I can visit the residnt index page and see an edit button next to each resident" do
+      it "will take the user to the edit page for that shelter" do
+        arlington_life = Shelter.create!(shelter_name: "Arlington Life Shelter", address: "lkahlsdkfh", case_management_available: true, capasity: 50)
+        austin_street = Shelter.create!(shelter_name: "Austin Street Shelter", address: "lkahlsdkfh", case_management_available: false, capasity: 25) 
+        jones = Resident.create!(family_name: "Jones", long_term_housing_need: true, family_size: 4, shelter_id: "#{arlington_life.id}")
+        visit "/residents/#{jones.id}/edit"
+        expect(page).to have_content("Family Name")
+        expect(page).to have_content("Family Size")
+        fill_in('family_name', with: "Test1")
+        fill_in('long_term_housing_need', with: "true")
+        fill_in('family_size', with: "3")
+        click_button('Update Resident Record')
+        expect(page).to have_content("Test1")
+        expect(page).to have_content("Long Term Housing Need: true")
+        expect(page).to have_content("Family Size: 3")
+      end
+    end
+  end
 
 
 end
--- spec/features/residents/index_spec.rb
+++ spec/features/residents/index_spec.rb
@@ -63,9 +63,18 @@ RSpec.describe "Resident Index", type: :feature do
         arlington_life = Shelter.create!(shelter_name: "Arlington Life Shelter", address: "lkahlsdkfh", case_management_available: true, capasity: 50)
         austin_street = Shelter.create!(shelter_name: "Austin Street Shelter", address: "lkahlsdkfh", case_management_available: false, capasity: 25) 
         jones = Resident.create!(family_name: "Jones", long_term_housing_need: true, family_size: 4, shelter_id: "#{arlington_life.id}")
-        bradley = Resident.create!(family_name: "Bradley", long_term_housing_need: false, family_size: 2, shelter_id: "#{austin_street.id}")
         visit "/residents"
         expect(page).to have_content("Edit Resident Record")
+        click_link("Edit Resident Record")
+        expect(page).to have_content("Family Name")
+        expect(page).to have_content("Family Size")
+        fill_in('family_name', with: "Test1")
+        fill_in('long_term_housing_need', with: "true")
+        fill_in('family_size', with: "3")
+        click_button('Update Resident Record')
+        expect(page).to have_content("Test1")
+        expect(page).to have_content("Long Term Housing Need: true")
+        expect(page).to have_content("Family Size: 3")
       end
     end
   end
